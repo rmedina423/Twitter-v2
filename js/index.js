bc127ad014baba6d4f29b35f01ff7f292f3f5643
Ryan M. Medina <rmedina423@gmail.com>
2015-07-15 22:11:19 -0700

    request replies
    get replies and attach to corresponding tweets

--- js/index.js
+++ js/index.js
@@ -11,25 +11,6 @@ $(function () {
 	$('#tweets').on('click','.tweet', function () {
 		$(this).parent().toggleClass('expand')
 	})
-	
-/*	$.getJSON('http://localhost:3000/tweets')
-		.done(function (tweets) {
-			tweets.forEach(function (tweet) {
-				// console.log('tweet ' + tweet.id + ' belongs to user ' + tweet.userId)
-					$.getJSON('http://localhost:3000/users/' + tweet.userId)
-						.done(function (user) {
-							// console.log(user.handle + ' created tweet ' + tweet.message)
-							var user = {
-								handle: user.handle,
-								img: user.img
-							}
-							var message = tweet.message
-							var id = tweet.id
-
-							$('#tweets').append(renderThread(user, message, id))
-						})
-			})
-		})*/
 
 	var baseUrl = 'http://localhost:3000'
 
@@ -37,20 +18,8 @@ $(function () {
     	return $.get(baseUrl + '/users')
   	}
 
-/*  	function getTweets(userTweets) {
-  		userTweets.forEach(function (tweet) {
-  			var userData = {
-  					handle: user.handle,
-					img: user.img
-					}
-			var message = tweet.message
-			var id = tweet.id
-  		})
-  	}*/
-
 	function getUserTweets(users) {
 		users.forEach(function (user) {
-			// console.log('user data', user)
 			$.get(baseUrl + '/users/' + user.id + '/tweets')
 			.done(function (userTweets) {
 				userTweets.forEach(function (tweet) {
@@ -61,18 +30,18 @@ $(function () {
 					var message = tweet.message
 					var id = tweet.id
 
-		 			$('#tweets').append(renderThread(userData, message, id))
- 					getUserReplies(users)
+		 			var html = renderThread(userData, message, id)
+
+		 			$('#tweets').append(html)
+
 				})
-			})
-			.fail(function (xhr) {
-				console.log('user ' + user.id + ' posts request failed', xhr.status)
 			})
 		})
 	}
 
 	function getUserReplies(users) {
 		users.forEach(function (user) {
+			// console.log(user)
 			$.get(baseUrl + '/users/' + user.id + '/replies/')
 				.done(function (replies) {
 					replies.forEach(function (reply) {
@@ -82,57 +51,21 @@ $(function () {
 								img: user.img
 							}
 						var message = reply.message
-						var search = $('#tweets').find('#tweet-' + reply.tweetId).siblings('.replies')
+						var id = reply.tweetId
+						var search = $('#tweets').find('#tweet-' + id).siblings('.replies')
 
 						// search.append(renderThread(reply))
-						search.append(renderTweet(userData, message))
+						var html = renderTweet(userData, message, id)
 
+						search.append(html)
 
 					})
 				})
 		})
 	}
 
-	 getUsers()
-	    .done(getUserTweets)
-
-	   //  .fail(function (xhr) {
-	   //  	console.log('users request failed', xhr.status)
-    // })
-
-		// $.get('http://localhost:3000/users')
-		// 	.done(function (users) {
-		// 		users.forEach(function (user) {
-		// 			$.get('http://localhost:3000/users/' + user.id + '/tweets')
-		// 				.done(function (userTweets) {
-		// 					userTweets.forEach(function (tweet) {
-		// 						// console.log(user.handle + ' created tweet ' + tweet.message)
-
-		// 						var userData = {
-		// 							handle: user.handle,
-		// 							img: user.img
-		// 						}
-		// 						var message = tweet.message
-		// 						var id = tweet.id
-
-		// 						$('#tweets').append(renderThread(userData, message, id))
-		// 					})
-		// 			$.get('http://localhost:3000/replies')
-		// 				.done(function (replies) {
-		// 					replies.forEach(function (reply) {
-		// 						console.log(reply)
-		// 						// console.log('reply ' + reply.id + ' was made by user ' + reply.userId + ' and sent to tweet ' + reply.tweetId)
-		// 						var search = $('#tweets').find('#tweet-' + reply.userId)
-
-		// 						console.log(search)
-
-		// 						// search.append(reply)
-		// 					})
-		// 				})
-		// 			})
-		// 		})
-		// })
-	
+	getUsers().done(getUserTweets)
+	getUsers().done(getUserReplies)
 
 
 	function renderTweet(user, message, id) {
