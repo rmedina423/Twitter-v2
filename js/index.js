ccae4a16b4964c11eee713cecbbf520fd649af39
Ryan M. Medina <rmedina423@gmail.com>
2016-08-07 22:27:32 -0700

    clean code

--- js/index.js
+++ js/index.js
@@ -1,192 +1,148 @@
-/* jshint node: true, asi: true */
-'use strict'
+(function () {
+	'use strict';
+}());
 
 var $ = require('jquery');
-var template = require('./template.js')
+var template = require('./template.js');
+var baseUrl = 'http://localhost:3000';
+var getUsers = $.get(baseUrl + '/users/');
+var tweetsContainer = $('#tweets');
+var mainContainer = $('#main');
+var currentUser = $('#currentUser');
 
 var user = {
 	id: 4,
 	img: "images/ryan.jpg",
 	handle: "@ryanmedina",
 	realName: "Ryan M. Medina",
+};
+
+$( document ).ready(function () {
+	getUsers.done(getUserTweets).done(getUserReplies);
+	attachEvents();
+});
+
+function getUserTweets(users) {
+	users.forEach(function (user) {
+		$.get(getUserTweetsURL(user.id)).done(function (userTweets) {
+			getTweets(userTweets, user);
+		});
+	});
 }
 
-$(function () {
+function getUserTweetsURL(userID) {
+	return baseUrl + '/users/' + userID + '/tweets';
+}
 
-	$('#currentUser').on('change', function () {
-		$.get('http://localhost:3000/users')
-			.done(function (users) {
-				users.forEach(function (userx) {
-					if (userx.id == $('#currentUser').val()) {
-						user = userx
-					}
-				})
-			})
-	})
+function getTweets(userTweets, user) {
+	userTweets.forEach(function (tweet) {
+		tweetsContainer.append(renderThread({
+			handle: user.handle,
+			img: user.img
+		}, tweet.message, tweet.id));
+	});
+}
 
-	var baseUrl = 'http://localhost:3000'
+function getUserReplies(users) {
+	users.forEach(function (user) {
+		$.get(getUserRepliesURL(user.id)).done(function (replies) {
+			replies.forEach(function (reply) {
+				if (user.id === reply.userId) {								
+					var search = tweetsContainer.find('#tweet-' + reply.tweetId).siblings('.replies');
 
-	var getUsers = $.get(baseUrl + '/users/')
+					search.append(renderTweet({
+						handle: user.handle,
+						img: user.img
+					}, reply.message, reply.tweetId));
+				}
+			});
+		});
+	});
+}
 
-	function getUserTweets(users) {
-		users.forEach(function (user) {
-			(function (currentUser) {
-				$.get(baseUrl + '/users/' + currentUser.id + '/tweets')
-					.done(function (userTweets) {
-						getTweets(userTweets, currentUser)
-					})
-			}(user))
-		})
-	}
+function getUserRepliesURL(userID) {
+	return baseUrl + '/users/' + userID + '/replies/';
+}
 
-	function getTweets(userTweets, currentUser) {
-		userTweets.forEach(function (tweet) {
-			var userData = {
-				handle: currentUser.handle,
-				img: currentUser.img
+function renderTweet(user, message, id) {
+	return template.tweet({
+		user: user,
+		id: id,
+		message: message
+	});
+}
+
+function renderThread(user, message, id) {
+	return template.thread({
+		tweet: renderTweet(user, message, id),
+		compose: template.compose
+	});
+}
+
+function attachEvents() {
+	mainContainer.on('click', 'textarea', onClickExpandTextArea);
+	currentUser.on('change', onChangeCurrentUser);
+	tweetsContainer.on('click','.tweet', onClickExpandTweet);
+	mainContainer.on('click', 'button', onClickSendButton);
+	mainContainer.on('keyup', 'textarea', onKeyUpTextArea);
+}
+
+function onChangeCurrentUser() {
+	$.get('http://localhost:3000/users').done(function (users) {
+		users.forEach(function (currentUser) {
+			if (currentUser.id == currentUser.val()) {
+				user = currentUser;
 			}
+		});
+	});
+}
 
-			var message = tweet.message
-			var id = tweet.id
+function onClickExpandTextArea() {
+	$(this).parent().addClass('expand');
+	$(this).attr('maxlength', '140');
+}
 
-			var html = renderThread(userData, message, id)
+function onClickExpandTweet() {
+	$(this).parent().toggleClass('expand');
+}
 
-			$('#tweets').append(html)
+function onClickSendButton(event) {
+	event.preventDefault();
 
-		})
-	}
+	var btnClicked = $(this);
+	var replyTweetLoc = btnClicked.parents('.replies');
+	var message = $(this).parents('.compose').find('textarea').val();
 
-	function getUserReplies(users) {
-		users.forEach(function (user) {
-			$.get(baseUrl + '/users/' + user.id + '/replies/')
-				.done(function (replies) {
-					replies.forEach(function (reply) {
-							if (user.id === reply.userId) {			
-								var userData = {
-									handle: user.handle,
-									img: user.img
-								}
-								
-								var message = reply.message
+	if ($(this).parents('.replies').length) {
+		var tweet = btnClicked.parents('.thread').find('.tweet:first-child').attr('id');
+		var tweetId = tweet.slice(6);
 
-								var id = reply.tweetId
+		replyTweetLoc.append(renderTweet(user, message));
 
-								var search = $('#tweets').find('#tweet-' + id).siblings('.replies')
-
-								var html = renderTweet(userData, message, id)
-
-								search.append(html)
-							}
-
-					})
-				})
-		})
-	}
-
-	function getReplies(replies, tweet) {
-					var userData = {
-							handle: user.handle,
-							img: user.img
-						}
-					
-					var message = reply.message
-
-					var id = reply.tweetId
-
-					var search = $('#tweets').find('#tweet-' + id).siblings('.replies')
-
-					var html = renderTweet(userData, message, id)
-
-					search.append(html)
-	}
-
-	getUsers
-		.done(getUserTweets)
-		.done(getUserReplies)
-
-
-	function renderTweet(user, message, id) {
-
-		var theData = {
-			user: user,
-			id: id,
+		$.post('http://localhost:3000/replies', {
+			userId: user.id,
+			tweetId: tweetId,
 			message: message
-		}
+		});
 
-		return template.tweet(theData)
+	} else {
+		tweetsContainer.append(renderThread(user, message));
+
+		$.post('http://localhost:3000/tweets', {
+			userId: user.id,
+			message: message,
+		});
 	}
 
-	function renderThread(user, message, id) {
+	$(this).parents('.compose').removeClass('expand');
+	$('textarea').val('');
+	$('.count').html('140');
+}
 
-		var theData = {
-			tweet: renderTweet(user, message, id),
-			compose: template.compose
-		}
+function onKeyUpTextArea() {
+	var counter = $(this).parent().find('.count');
+	var max = 140;
+	var value = $(this).val();
 
-		return template.thread(theData)
-	}
-
-	$('#main').on('click', 'textarea', function () {
-		$(this).parent().addClass('expand')
-		$(this).attr('maxlength', '140')
-	})
-
-	$('#tweets').on('click','.tweet', function () {
-		$(this).parent().toggleClass('expand')
-	})
-
-	$('#main').on('click', 'button', function (event) {
-		event.preventDefault()
-
-		var btnClicked = $(this)
-
-		var replyTweetLoc = btnClicked.parents('.replies')
-
-		var message = $(this).parents('.compose').find('textarea').val()
-
-
-		if ($(this).parents('.replies').length) {
-
-			var output = renderTweet(user, message)
-			var tweet = btnClicked.parents('.thread').find('.tweet:first-child').attr('id')
-
-			var tweetId = tweet.slice(6)
-
-			replyTweetLoc.append(output)
-
-			$.post('http://localhost:3000/replies', {
-				userId: user.id,
-				tweetId: tweetId,
-				message: message
-			})
-
-		} else {
-			var output = renderThread(user, message)
-
-			$('#tweets').append(output)
-
-			$.post('http://localhost:3000/tweets', {
-				userId: user.id,
-				message: message,
-			})
-		}
-
-		$(this).parents('.compose').removeClass('expand')
-
-		$('textarea').val('')
-
-		$('.count').html('140')
-		
-	})
-
-	$('#main').on('keyup', 'textarea', function () {
-			var counter = $(this).parent().find('.count')
-
-			var max = 140
-
-			var value = $(this).val()
-
-			counter.text(max-value.length)
-		})
-
-});
+	counter.text(max - value.length);
+}
